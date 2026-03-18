12c530316d4b69b445f8df4f911d3a9027475d62
Alexis Bouchez <alexisbouchez@users.noreply.github.com>
2026-03-18 16:25:29 +0100

    add clickhouse schema init in installation program

--- cmd/install/main.go
+++ cmd/install/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/alexisbouchez/hyperseer/internal/config"
+	"github.com/alexisbouchez/hyperseer/internal/db"
+	"github.com/alexisbouchez/hyperseer/internal/schema"
 )
 
 func main() {
-	fmt.Println("install: not implemented yet")
+	cfg := config.New()
+
+	conn, err := db.New(cfg.ClickHouse)
+	if err != nil {
+		log.Fatalf("clickhouse: %v", err)
+	}
+	defer conn.Close()
+
+	if err := schema.Migrate(conn); err != nil {
+		log.Fatalf("migrate: %v", err)
+	}
+
+	fmt.Println("\033[32m•\033[0m ClickHouse schema ready")
 }
