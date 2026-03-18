8ba0a01a780fcf9d2d1f2f6e5bd029ac49e1b22e
Alexis Bouchez <alexisbouchez@users.noreply.github.com>
2026-03-18 16:06:16 +0100

    add config package

---
+++ internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"strconv"
+
+	"github.com/alexisbouchez/hyperseer/internal/env"
+)
+
+type Config struct {
+	ClickHouse ClickHouseConfig
+	Serve      ServeConfig
+}
+
+type ClickHouseConfig struct {
+	Host     string
+	Port     int
+	Database string
+	Username string
+	Password string
+}
+
+type ServeConfig struct {
+	OTLPAddr  string
+	QueryAddr string
+}
+
+func New() Config {
+	port, _ := strconv.Atoi(env.GetVar("HYPERSEER_CH_PORT", "9000"))
+	return Config{
+		ClickHouse: ClickHouseConfig{
+			Host:     env.GetVar("HYPERSEER_CH_HOST", "localhost"),
+			Port:     port,
+			Database: env.GetVar("HYPERSEER_CH_DATABASE", "hyperseer"),
+			Username: env.GetVar("HYPERSEER_CH_USERNAME", "hyperseer"),
+			Password: env.GetVar("HYPERSEER_CH_PASSWORD", "hyperseer"),
+		},
+		Serve: ServeConfig{
+			OTLPAddr:  env.GetVar("HYPERSEER_OTLP_ADDR", ":4317"),
+			QueryAddr: env.GetVar("HYPERSEER_QUERY_ADDR", ":7777"),
+		},
+	}
+}
